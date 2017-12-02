94acb209e20b7191523839fb285bb762842e0189
yangqingming <a413968232@live.com>
2017-12-03 00:25:14 +0800

    add file

---
+++ Assets/ClientApp/Editor/GameFrameworkConfigs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameFrameworkConfigs
+{
+    //[BuildSettingsConfigPath]
+    //public static string BuildSettingsConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/BuildSettings.xml");
+
+    //[AssetBundleBuilderConfigPath]
+    //public static string AssetBundleBuilderConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleBuilder.xml");
+
+    //[AssetBundleEditorConfigPath]
+    //public static string AssetBundleEditorConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleEditor.xml");
+
+    //[AssetBundleCollectionConfigPath]
+    //public static string AssetBundleCollectionConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleCollection.xml");
+}
