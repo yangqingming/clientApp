55df1a0c660ab626c689ad84d8153ff3c05a11c9
yangqingming <a413968232@live.com>
2017-12-03 22:38:33 +0800

    增加assetbundle编辑器配置

--- Assets/ClientApp/Editor/GameFrameworkConfigs.cs
+++ Assets/ClientApp/Editor/GameFrameworkConfigs.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityGameFramework.Editor.AssetBundleTools;
 public static class GameFrameworkConfigs
 {
     //[BuildSettingsConfigPath]
     //public static string BuildSettingsConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/BuildSettings.xml");
 
-    //[AssetBundleBuilderConfigPath]
-    //public static string AssetBundleBuilderConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleBuilder.xml");
+    [AssetBundleBuilderConfigPath]
+    public static string AssetBundleBuilderConfig = Application.dataPath + "/ClientApp/Configs/AssetBundleBuilder.xml";
 
-    //[AssetBundleEditorConfigPath]
-    //public static string AssetBundleEditorConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleEditor.xml");
+    [AssetBundleEditorConfigPath]
+    public static string AssetBundleEditorConfig = Application.dataPath + "/ClientApp/Configs/AssetBundleEditor.xml";
 
     //[AssetBundleCollectionConfigPath]
     //public static string AssetBundleCollectionConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleCollection.xml");
